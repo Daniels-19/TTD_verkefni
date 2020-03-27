2111da16c4619ca5c6f6271ed4f7d2b574f04a9d
Daniels-19 <daniels19@ru.is>
2020-03-27 11:54:16 +0000

    hluti 3 uppfylltur

--- kata.py
+++ kata.py
@@ -3,4 +3,8 @@ def add(a_str):
     if a_str == "":
         return 0
     else:
-        return int(a_str)
+        ret_num = 0
+        a_list = a_str.split(",")
+        for num in a_list:
+            ret_num += int(num)
+        return int(ret_num)
--- test.py
+++ test.py
@@ -7,3 +7,7 @@ def test():
     # hluti 2
     assert add("1") == 1
     assert add("10") == 10
+
+    # hluti 3
+    assert add("1,2") == 3
+    assert add("10,20") == 30
